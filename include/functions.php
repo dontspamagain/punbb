716415d29ae749710be2264f0fb9eddb9c8693cb
Visman <visman2008@googlemail.com>
2019-11-22 20:35:59 +0700

    Banned title takes precedence
    Before the change, the сustom title was displayed even when the user’s
    ban.

--- include/functions.php
+++ include/functions.php
@@ -1050,12 +1050,12 @@ function get_title($user)
 		}
 	}
 
-	// If the user has a custom title
-	if ($user['title'] != '')
-		$user_title = forum_htmlencode($forum_config['o_censoring'] == '1' ? censor_words($user['title']) : $user['title']);
 	// If the user is banned
-	else if (in_array(utf8_strtolower($user['username']), $ban_list))
+	if (in_array(utf8_strtolower($user['username']), $ban_list))
 		$user_title = $lang_common['Banned'];
+	// If the user has a custom title
+	else if ($user['title'] != '')
+		$user_title = forum_htmlencode($forum_config['o_censoring'] == '1' ? censor_words($user['title']) : $user['title']);
 	// If the user group has a default user title
 	else if ($user['g_user_title'] != '')
 		$user_title = forum_htmlencode($user['g_user_title']);

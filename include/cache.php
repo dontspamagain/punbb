85a9a885db38987475074c285b19b3e19b58cfc2
dontspamagain <dontspamagain@gmail.com>
2024-01-01 04:31:28 +0100

    .
    Some ajustements

--- include/cache.php
+++ include/cache.php
@@ -404,7 +404,7 @@ function clean_quickjump_cache($group_id = false)
 		// A group_id was not supplied, so we generate the quickjump cache for all groups
 		$query = array(
 			'SELECT'	=> 'g.g_id',
-			'FROM'		=> 'groups AS g'
+			'FROM'		=> '`groups` AS g'
 		);
 
 		($hook = get_hook('ch_fn_clean_quickjump_cache_qr_get_groups')) ? eval($hook) : null;
